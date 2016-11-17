5a24a7fafabb5a29b02212e6d24e9dcacca3ae33
malilasage <malilaclearwater@gmail.com>
2016-11-17 10:19:25 -0700

    sickk

--- script.js
+++ script.js
@@ -1,64 +1,73 @@
 $(document).ready(function(){
-  console.log('cool');
+console.log('cool');
 var userInput;
 var arr = ['rock', 'paper', 'scissors'];
 var winner;
 var $result = $('#result');
 
-$('#game-container').on('click', function() {
-  var et = event.target;
-  if(et.hasClass('rock')) {
+$('#game-container').on('click', function(event) {
+  var et = $(event.target);
+  if(et.has('rock')) {
+    console.log('super ok');
     userInput = 'rock';
   }
-  else if(et.hasClass('paper')) {
+  else if(et.has('paper')) {
     userInput = 'paper';
   }
-  else if(et.hasClass('scissors')){
+  else if(et.has('scissors')){
     userInput = 'scissors';
   }
   compare(userInput);
 })
 
 function compare(userInput) {
-  // var compInput = arr[Math.floor(Math.random() * 3)];
-  var compInput = 'scissors';
+  console.log('in here');
+  console.log(userInput);
+  var compInput = arr[Math.floor(Math.random() * 3)];
   if(userInput === 'rock') {
     if(compInput === 'scissors') {
+      console.log('ok;')
       win();
     }
     else if(compInput === 'rock') {
-      //tie
+      tie();
     }
     else {
-      //lose
+      lose();
     }
   }
   else if(userInput === 'scissors') {
     if(compInput === 'paper') {
-      //win
+      win();
     }
     else if(compInput === 'scissors') {
-      //tie
+      tie();
     }
     else {
-      //lose
+      lose();
     }
   }
   else if(userInput === 'paper') {
     if(compInput === 'rock') {
-      //win
+      win();
     }
     else if(compInput === 'paper') {
-      //tie
+      tie();
     }
     else if(compInput === 'scissors') {
-      //lose
+      lose();
     }
   }
 }
 
 
 function win() {
-  result.text('You won');
+  $result.text('You won');
+}
+function tie() {
+  $result.text('It is a tie');
+}
+function lose() {
+  $result.text('You lost');
 }
 });
